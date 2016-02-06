7ce082bba152c7dc30baf6df46fa0ec4e103b04e
Ryosuke ONOSE <mail@onose.ws>
2016-02-06 17:15:00 +0900

    1日目終了👏

--- noseApp/src/ofApp.cpp
+++ noseApp/src/ofApp.cpp
@@ -2,12 +2,20 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    
+    ofSetCircleResolution(100);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    angle += 0.1;
+    
+//    x = 100 * cos(angle);
+//    y = 100 * sin(angle);
+    
+    x += a * vx;
+    y += a * vy;
+    if(x >= ofGetWidth() || x <= 0) vx *= -1;
+    if(y >= ofGetHeight() || y <= 0) vy *= -1;
 }
 
 //--------------------------------------------------------------
@@ -38,30 +46,56 @@ void ofApp::draw(){
 //    ofEndShape();
 //
     // make grid
+    
+    
+    easycam.begin();
+    
     int gridXNum = 8;
     int gridYNum = 6;
     
     int gridX = ofGetWidth() / gridXNum;
     int gridY = ofGetHeight() / gridYNum;
     
+    ofSetColor(100, 100, 100);
     for (int i = 0; i < gridXNum; i++) {
         ofLine(i * gridX, 0, i * gridX, ofGetHeight());
     }
     for (int i = 0; i < gridYNum; i++) {
         ofLine(0, i * gridY, ofGetWidth(), i * gridY);
     }
-    
+    ofSetColor(255, 0, 0);
     // draw
-    ofEllipse(gridX * 1, gridY * 3, gridX * 1.5, gridY * 5);
-    ofCircle(gridX * 3, gridY * 1, gridX * 0.6);
-    ofRect(gridX * 4, gridY * 2, gridX * 2, gridY * 2);
-    ofTriangle(gridX * 2.3, gridY * 4.3, gridX * 2.3, gridY * 5.6, gridX * 3.6, gridY * 5.6);
+//    ofEllipse(gridX * 1, gridY * 3, gridX * 1.5, gridY * 5);
+    ofCircle(x, y, gridX * 0.6);
+//    ofRect(gridX * 4, gridY * 2, gridX * 2, gridY * 2);
+//    ofColor color = ofColor(255, 0, 0);
+//    ofSetColor(color);
+//    ofTriangle(gridX * 2.3, gridY * 4.3, gridX * 2.3, gridY * 5.6, gridX * 3.6, gridY * 5.6);
     
+    ofPushMatrix();
+    
+    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+    ofRotate(45);
+    ofRect(-100, -100, 200, 200);
+    
+    ofPopMatrix();
+    ofRect(200, 200, 200, 200);
+    
+    easycam.end();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    switch(key){
+        case 'a':
+            a += 1;
+            break;
+        case 'b':
+            a -= 1;
+            break;
+        default:
+            break;
+    }
 }
 
 //--------------------------------------------------------------
