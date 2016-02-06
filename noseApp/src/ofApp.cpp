9ac1fddb711f68f2f7f445bae25790cb15055caa
Ryosuke ONOSE <mail@onose.ws>
2016-02-06 16:00:20 +0900

    練習問題 1-1 できました！

--- noseApp/src/ofApp.cpp
+++ noseApp/src/ofApp.cpp
@@ -15,30 +15,47 @@ void ofApp::draw(){
     ofBackground(0, 0, 0);
     ofSetColor(255, 255, 255);
     
-    ofFill();
+//    ofFill();
+//    
+//    ofCircle(300, 250, 100);
+//    ofRect(200, 450, 200, 200);
+//    ofTriangle(100, 100, 100, 200, 150, 150);
+//    ofEllipse(500, 500, 100, 200);
+//    ofLine(0, 0, 300, 400);
+//    
+//    
+//    ofNoFill();
+//    ofSetLineWidth(5.0);
+//    ofBeginShape();
+//    ofVertex(30, 30);
+//    ofVertex(40, 35);
+//    ofVertex(50, 100);
+//    ofVertex(60, 45);
+//    ofVertex(70, 50);
+//    ofVertex(80, 55);
+//    ofVertex(90, 60);
+//    ofVertex(190, 95);
+//    ofEndShape();
+//
+    // make grid
+    int gridXNum = 8;
+    int gridYNum = 6;
     
-    ofCircle(300, 250, 100);
-    ofRect(200, 450, 200, 200);
-    ofTriangle(100, 100, 100, 200, 150, 150);
-    ofEllipse(500, 500, 100, 200);
-    ofLine(0, 0, 300, 400);
+    int gridX = ofGetWidth() / gridXNum;
+    int gridY = ofGetHeight() / gridYNum;
     
+    for (int i = 0; i < gridXNum; i++) {
+        ofLine(i * gridX, 0, i * gridX, ofGetHeight());
+    }
+    for (int i = 0; i < gridYNum; i++) {
+        ofLine(0, i * gridY, ofGetWidth(), i * gridY);
+    }
     
-    ofNoFill();
-    ofSetLineWidth(5.0);
-    ofBeginShape();
-    ofVertex(30, 30);
-    ofVertex(40, 35);
-    ofVertex(50, 100);
-    ofVertex(60, 45);
-    ofVertex(70, 50);
-    ofVertex(80, 55);
-    ofVertex(90, 60);
-    ofVertex(190, 95);
-    ofEndShape();
-    
-    
-    
+    // draw
+    ofEllipse(gridX * 1, gridY * 3, gridX * 1.5, gridY * 5);
+    ofCircle(gridX * 3, gridY * 1, gridX * 0.6);
+    ofRect(gridX * 4, gridY * 2, gridX * 2, gridY * 2);
+    ofTriangle(gridX * 2.3, gridY * 4.3, gridX * 2.3, gridY * 5.6, gridX * 3.6, gridY * 5.6);
     
 }
 
